f03da53862b06013dfcad80fd4962e5d6db0214c
monicamulas <99686908+monicamulas@users.noreply.github.com>
2022-02-14 20:58:21 +0100

    Update Dow Jones Index.py

--- Dow Jones Index.py
+++ Dow Jones Index.py
@@ -4,98 +4,98 @@ Created on Tue Jun 22 09:58:44 2021
 
 @author: Monica
 """
-#importo le librerie necessarie
-import numpy as np #mi permette di lavorare con gli array
-import matplotlib.pyplot as plt #mi permette di mostrare graficamente i risultati ottenuti
+#importing necessary libraries
+import numpy as np 
+import matplotlib.pyplot as plt 
 
-#trasformata di Fourier numerica
+#numerical Fourier transform
 def Fourier_transform(N, h, tau): 
-    H = np.zeros(N, dtype = "complex") #salvo nell'array valori complessi
+    H = np.zeros(N, dtype = "complex") #array filled with complex numbers
     for n in range(N):
         for k in range(N):
             r = n / N
             H[n] += h[k] * np.exp(2j * np.pi * k * r)
     H *= tau
     freq = np.arange(N) / tau / N
-    return H, freq #trasformata H nello spazio duale (frequenze)
+    return H, freq #H transform in frequency space
 
-#anti-trasformata di Fourier numerica
+#numerical anti-Fourier transform
 def anti_Fourier_transform(N, H, tau):
-    h = np.zeros(N, dtype = "complex") #salvo nell'array valori complessi
+    h = np.zeros(N, dtype = "complex") 
     for k in range(N):
         for n in range(N):
             r = n / N
             h[k] += H[n] * np.exp(-2j * np.pi * k * r)
     H *= 1 / tau / N
-    return h #anti-trasformata h nello spazio originario (tempi)
+    return h #anti-tranform in time space
 
-h = [] #definisco una lista h
+h = [] 
 
-file = open("dow2.txt", "r") #apro il file dow2.txt in modalità lettura
+file = open("dow2.txt", "r") 
 data = file.readlines() 
 for line in data:
     h.append(float(line)) #salvo i dati contenuti nel file nella variabile h
     
 N = len(h)
-t = np.arange(len(h)) #definisco i tempi
-h = np.array(h) #trasformo le liste in array
-tau = 1 / N #definizione dello step temporale
+t = np.arange(len(h)) #time
+h = np.array(h) 
+tau = 1 / N #timestep
 
-H, freq = Fourier_transform(N, h, tau) #calcolo la trasformata
+H, freq = Fourier_transform(N, h, tau) 
 
-#definisco una funzione che pone tutte le entrate di H uguali a zero tranne il primo 2%
+#defining a function that puts all H entries as zero but the first 2%
 def keepdata(H):
-    H_split = np.array_split(H, 50) #faccio uno split di H in 50 array (ognuno conterrà un 2%)
-    new_H = np.zeros(len(H), dtype = "complex") #definisco un array complesso di zeri 
+    H_split = np.array_split(H, 50) #H is split in 50 arrays (every one has a 2%)
+    new_H = np.zeros(len(H), dtype = "complex") 
     for i in range(len(H_split[0])):
-        new_H[i] = H_split[0][i] #salvo i valori del primo 2% all'inizio dell'array
+        new_H[i] = H_split[0][i] 
     return new_H
 
-new_h = anti_Fourier_transform(N, keepdata(H), tau) #calcolo l'anti-trasformata con il nuovo array di H
+new_h = anti_Fourier_transform(N, keepdata(H), tau) #anti-transform only using 2% of data
 
 
-#faccio gli stessi passaggi con la FFT per vedere le differenze
+#same process with fast Fourier transform
 nu, H_fast = np.fft.rfftfreq(len(t)), np.fft.rfft(h)
 new_h_fast = np.fft.irfft(keepdata(H_fast))
 
 
-#plotto la trasformata di Fourier
+#graphs
 fig, ax = plt.subplots()
-ax.plot(freq[:N//2], abs(H[:N//2]), color = "b", label = "without windowing") #frequenze positive
-ax.plot(freq[N//2:] - N, abs(H[N//2:]), color = "b") #frequenze negative
+ax.plot(freq[:N//2], abs(H[:N//2]), color = "b", label = "without windowing") #positive frequencies
+ax.plot(freq[N//2:] - N, abs(H[N//2:]), color = "b") #negative frequencies
 ax.set_xlabel("$\\nu$ (1/days)")
 ax.set_ylabel("Fourier coefficients")
 ax.set_title("Fourier transform")
 ax.legend()
 
 fig1, ax1 = plt.subplots()
-ax1.plot(freq[:N//2], abs(H[:N//2]), color = "b") #frequenze positive
-ax1.plot(freq[N//2:] - N, abs(H[N//2:]), color = "b") #frequenze negative
+ax1.plot(freq[:N//2], abs(H[:N//2]), color = "b") 
+ax1.plot(freq[N//2:] - N, abs(H[N//2:]), color = "b") 
 ax1.set_yscale("log")
 ax1.set_xlabel("$\\nu$ (1/days)")
 ax1.set_ylabel("Fourier coefficients")
 ax1.set_title("Fourier transform")
 
 
-#definisco la funzione finestra di Hann per diminuire il fenomeno di leakage
+#defining of Hann window function to reduce leakage
 Hann_function = 0.5 - 0.5 * np.cos(2 * np.pi * t / t[-1])
 h_windowed = h * Hann_function
 H_windowed, frequenze = Fourier_transform(N, h_windowed, tau) 
 new_h_windowed = anti_Fourier_transform(N, H_windowed, tau)
 
-#plotto la trasformata applicando il windowing
+#plot the transform using windowing
 fig2, ax2 = plt.subplots()
-ax2.plot(freq[:N//2], abs(H[:N//2]), color = "b", label = "without windowing") #frequenze positive
-ax2.plot(freq[N//2:] - N, abs(H[N//2:]), color = "b") #frequenze negative
-ax2.plot(frequenze[:N//2], abs(H_windowed[:N//2]), color = "r", label = "with windowing") #frequenze positive
-ax2.plot(frequenze[N//2:] - N, abs(H_windowed[N//2:]), color = "r") #frequenze negative
+ax2.plot(freq[:N//2], abs(H[:N//2]), color = "b", label = "without windowing") #positive frequencies
+ax2.plot(freq[N//2:] - N, abs(H[N//2:]), color = "b") #negative frequencies
+ax2.plot(frequenze[:N//2], abs(H_windowed[:N//2]), color = "r", label = "with windowing") #positive frequencies
+ax2.plot(frequenze[N//2:] - N, abs(H_windowed[N//2:]), color = "r") #negative frequencies
 ax2.set_yscale("log")
 ax2.set_xlabel("$\\nu$ (1/days)")
 ax2.set_ylabel("Fourier coefficients")
 ax2.set_title("Fourier transform")
 ax2.legend()
 
-#plotto i dati originali a confronto con l'anti-trasformata
+#original data vs anti_Fourier transform
 fig3, ax3 = plt.subplots()
 ax3.plot(t, h, label = "Original data", color = "r")
 ax3.plot(t, abs(new_h), label = "Inverse trasform", color = "c", linewidth = 2)
